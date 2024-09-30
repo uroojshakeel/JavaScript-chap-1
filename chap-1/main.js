bdf96c07b20b75078e6f84dc0cbf06b40d0939eb
uroojshakeel <165478020+uroojshakeel@users.noreply.github.com>
2024-10-01 02:13:32 +0500

    Add files via upload

---
+++ chap-1/main.js
@@ -0,0 +1,37 @@
+        //Q1 
+// What does the alert function do in JavaScript? 
+
+// ans
+// An alert is a box that pops up to give the user a message.
+// alerting the user to important information. It pauses the execution 
+// of the script until the user clicks "OK".
+
+
+
+        //Q2
+ // Write the code to display an alert with the message "Welcome to my website!". 
+
+ alert("welcome to my website!");
+
+
+        //Q3
+// What happens if you capitalize the alert keyword?
+
+// ans
+// -Using Alert or ALERT will give you a syntax error.
+// - JavaScript is a case-sensitive language.
+// -The alert keyword must be in lowercase.
+
+
+
+        //Q4    
+ // Write the correct JavaScript syntax to display "Hello, world!" in an alert box. 
+//  Write a JavaScript alert statement with proper formatting and explain each part of the code. 
+
+// ans
+alert("urooj shakeel");
+// alert: A built-in JavaScript function to show a pop-up message.
+//  (: Opens the function to add the message.
+// "urooj shakeel": The message to display in the alert box.
+// ): Closes the function.
+// ;: Ends the statement.
